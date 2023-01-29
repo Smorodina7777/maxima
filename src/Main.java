bb4292a9b5596cb14d1a8f82d898ce3fec67f2b6
Smorodina7777 <gorlanova.elena@gmail.com>
2023-01-29 18:40:09 +0700

    метод подсчета кораблей по размерам для морского боя.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        // двоичные числа
+      /*byte x = 0b10; //зпись двоичного числа
+       byte y = 0b11;
+       int a = scanner.nextInt(2); // вврд двоичного числа
+       int c = scanner.nextInt(2);
+        int d = a*c;
+        String s = "110";
+        int et = Integer.parseInt(s, 2); // перевод строики в двоичное число
+        System.out.println(Integer.toBinaryString(d)); // вывод двоичного числа из int
+        System.out.println(Integer.toBinaryString(et));
+        System.out.println(a); //вывод десятичного числа из двоичного*/
+//шестнадцатиричные числа
+       /* int num = scanner.nextInt();
+        int num2 = num;
+        String s = "";
+        int oststok;
+        String ostV16="";
+        while (num>0){
+            oststok = num%16;
+            if (oststok ==15){
+                ostV16 = "F";
+            } else if (oststok ==14) {
+                ostV16 = "E";
+            } else if (oststok ==13) {
+                ostV16 = "D";
+            } else if (oststok ==12) {
+                ostV16 = "C";
+            } else if (oststok ==11) {
+                ostV16 = "B";
+            } else if (oststok ==10) {
+                ostV16 = "A";
+            } else if (oststok <=9) {
+                ostV16= Integer.toString(oststok);
+            }
+            s=ostV16+s;
+            num/=16;
+        }
+String stringNum=Integer.toString(num2, 16);
+        System.out.println(s);
+        System.out.println(stringNum);*/
+        //восьмеричное число
+        /*int num = scanner.nextInt();
+        String stringNum8 = Integer.toString(num, 8);
+        System.out.println(stringNum8);*/
+//проверка java на компьютере
+        /*System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.runtime.version"));
+        System.out.println(System.getProperty("java.home"));
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.class.path"));*/
+//площвдь многоугольника, где а - длина стороны, n - кол. сторон
+        /*int a= 6;
+        int n = 7;
+        double S = n * Math.pow(a, 2) / (4 * Math.tan(Math.PI / n));
+        System.out.println(S);*/
 
 
-            int[][] field = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+//подсчет кораблей для морского боя
+            /*int[][] field = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                 {0, 1, 1, 0, 0, 0, 1, 0, 0, 1},
                 {0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
                 {1, 1, 1, 1, 0, 0, 1, 0, 0, 0},
@@ -19,7 +79,7 @@ public class Main {
         int cnt4 = 0;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i < 9 && j < 9 && field[i][j] == 1 && field[i + 1][j] == 0 && field[i][j + 1] == 0) {
+                if (i < 9 && j < 9 && field[i][j] == 1 && field[i + 1][j] !=1 && field[i][j + 1] !=1) {
                     int cnt = 1;
                     for (int a = i-1; a>i-4; a--) {
 
@@ -49,7 +109,7 @@ public class Main {
                         cnt1++;
                     }
                 }
-                if (i == 9 && j < 9 && field[i][j] == 1 && field[i][j + 1] == 0) {
+                if (i == 9 && j < 9 && field[i][j] == 1 && field[i][j + 1] !=1) {
                     int cnt = 1;
                     for (int a = i-1; a>i-4; a--) {
 
@@ -80,7 +140,7 @@ public class Main {
                     }
 
                 }
-                if (i < 9 && j == 9 && field[i][j] == 1 && field[i + 1][j] == 0) {
+                if (i < 9 && j == 9 && field[i][j] == 1 && field[i + 1][j] !=1) {
                     int cnt = 1;
                     for (int a = i-1; a>i-4; a--) {
 
@@ -146,8 +206,11 @@ public class Main {
         System.out.println(cnt1);
         System.out.println(cnt2);
         System.out.println(cnt3);
-        System.out.println(cnt4);
-    }}
+        System.out.println(cnt4);*/
+
+    }
+
+}
 
 
 
