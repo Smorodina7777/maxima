42c4dab5ed0cbce84fdea48e5c524c53ca7a99dc
Елена Горланова <96808415+Smorodina7777@users.noreply.github.com>
2023-02-02 17:12:34 +0700

    Копир с логами

---
+++ src/lesson9/MainCopier.java
@@ -0,0 +1,54 @@
+package lesson9;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public class MainCopier {
+    public static void main(String[] args) {
+        try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby_копия.txt", false)) {
+            String nul = "";
+            writer.write(nul);
+            writer.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+
+        }
+        try (FileReader reader = new FileReader("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby.txt")) {
+            int c;
+            char[] buf = new char[256];
+            while ((c = reader.read(buf)) > 0) {
+
+                buf = Arrays.copyOf(buf, c);
+
+                try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby_копия.txt", true)) {
+                    writer.write(buf);
+                    writer.flush();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+
+                }
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+        try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\log.txt", true)) {
+            String text = DateTimeFormatter.ofPattern("yyyy-MMM-dd , hh:mm:ss a")
+                    .format(LocalDateTime.now()) + ". Был скопирован файл : Hobby.txt → Hobby_копия.txt\n";
+            writer.write(text);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
