027e7155b9543785548f7e81ef2dd63a5388224c
Елена Горланова <96808415+Smorodina7777@users.noreply.github.com>
2023-02-10 14:27:59 +0700

    копир с логами. исправила на readLine

--- src/lesson9/MainCopier.java
+++ src/lesson9/MainCopier.java
@@ -1,53 +1,34 @@
 package lesson9;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class MainCopier {
-    public static void main(String[] args) {
-        try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby_копия.txt", false)) {
-            String nul = "";
-            writer.write(nul);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-
-        }
-        try (FileReader reader = new FileReader("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby.txt")) {
-            int c;
-            char[] buf = new char[256];
-            while ((c = reader.read(buf)) > 0) {
-
-                buf = Arrays.copyOf(buf, c);
-
-                try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby_копия.txt", true)) {
-                    writer.write(buf);
-                    writer.flush();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-
-                }
-
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = null;
+        FileWriter writer2 = null;
+        try (FileReader fr = new FileReader("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby.txt")) {
+            writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\Hobby_копия.txt", false);
+            writer2 = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\log.txt", true);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                writer.write(line + "\n");
+                line = reader.readLine();
             }
-
-
+            String text = DateTimeFormatter.ofPattern("yyyy MMM dd , hh:mm:ss a")
+                    .format(LocalDateTime.now()) + ". Был скопирован файл : Hobby.txt → Hobby_копия.txt\n";
+            writer2.write(text);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-
-        }
-
-        try (FileWriter writer = new FileWriter("D:\\Users\\Lena\\Projects\\maxima\\src\\lesson9\\log.txt", true)) {
-            String text = DateTimeFormatter.ofPattern("yyyy-MMM-dd , hh:mm:ss a")
-                    .format(LocalDateTime.now()) + ". Был скопирован файл : Hobby.txt → Hobby_копия.txt\n";
-            writer.write(text);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+        } finally {
+            assert writer != null;
+            writer.close();
+            assert writer2 != null;
+            writer2.close();
         }
     }
 }
